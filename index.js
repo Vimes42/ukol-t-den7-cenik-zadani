39161a81f69462e40b88f37b15da7d096ddbe665
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-03-12 18:12:58 +0100

    splneny ukol

---
+++ index.js
@@ -0,0 +1,19 @@
+const selectPlan = (planNumber) => {
+    const plan1 = document.querySelector("#plan1")
+    const plan2 = document.querySelector("#plan2")
+    const plan3 = document.querySelector("#plan3")
+
+    if (planNumber === 1) {
+        plan1.classList.add("plan--selected")
+        plan2.classList.remove("plan--selected")
+        plan3.classList.remove("plan--selected")
+    } else if (planNumber === 2){
+        plan2.classList.add("plan--selected")
+        plan1.classList.remove("plan--selected")
+        plan3.classList.remove("plan--selected")
+    } else if (planNumber === 3) {
+        plan3.classList.add("plan--selected")
+        plan2.classList.remove("plan--selected")
+        plan1.classList.remove("plan--selected")
+    }
+}
